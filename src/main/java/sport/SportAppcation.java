f10b306820180e31f6a217a859e64916fa991034
wanghuangyong <w382259160@qq.com>
2018-09-22 17:02:10 +0800

    创建sport项目

---
+++ src/main/java/sport/SportAppcation.java
@@ -0,0 +1,13 @@
+package sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAppcation {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SportAppcation.class, args);
+    }
+}
